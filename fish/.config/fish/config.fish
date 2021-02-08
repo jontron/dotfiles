50b505ee27ae450f9d3d6433e5972f3d4e72386b
Jon Leopard <jontron2000@gmail.com>
2021-02-08 16:12:15 +0100

    Getting set up still...

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+# don't show any greetings
+set fish_greeting ""
+
+# don't describe the command for darwin
+# https://github.com/fish-shell/fish-shell/issues/6270
+function __fish_describe_command; end
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
+
+set -gx GOPATH $HOME/go
+set -gx PATH $PATH /usr/local/bin/go $GOPATH/bin
+
